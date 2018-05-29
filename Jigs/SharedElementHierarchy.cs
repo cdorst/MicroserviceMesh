71e40756f7b7e295a2adcf9caa831620dc2e45d3
Christopher Dorst <chris@dorst.email>
2018-05-29 08:48:59 -0500

    Add IHierarchy-type interfaces

--- Jigs/SharedElementHierarchy.cs
+++ Jigs/SharedElementHierarchy.cs
@@ -8,6 +8,9 @@ namespace Jigs
 {
     public static class SharedElementHierarchy
     {
+        private const string CloseAngleBrace = ">";
+        private const string Comma = ",";
+        private const string CommaSpace = ", ";
         private const string CurlyBraceClose = "}";
         private const string CurlyBraceOpen = "{";
         private const string Elements = nameof(Elements);
@@ -15,9 +18,98 @@ namespace Jigs
         private const byte MaxSameElements = 3;
         private const string Shared = nameof(Shared);
         private const string Tab = "\t";
+        private const string TDatum = nameof(TDatum);
+        private const string TDatumKey = nameof(TDatumKey);
+        private const string TDatumLabel = nameof(TDatumLabel);
+        private const string TDatumLabelKey = nameof(TDatumLabelKey);
+        private const string TDatumValue = nameof(TDatumValue);
+        private const string THierarchy = nameof(THierarchy);
+        private const string THierarchyKey = nameof(THierarchyKey);
+        private const string ThreeTabs = "\t\t\t";
+        private const string TwoTabs = "\t\t";
         private static readonly string _path = Combine(SolutionPath, Shared, Elements, Concat(IHierarchy, ".cs"));
 
         public static void WriteFile() => WriteAllText(_path, GetFileContent());
+
+        private static bool AppendCloseAngleBrace(in byte position, in byte last, in bool zeroHierarchies = true)
+            => zeroHierarchies && position == last;
+
+        private static bool AppendComma(in byte position, in byte last, in bool zeroHierarchies = true)
+            => position < last || (position == last && !zeroHierarchies);
+
+        private static StringBuilder AppendInterfaceGenericOverloads(this StringBuilder stringBuilder)
+        {
+            stringBuilder.AppendLine(Concat(Tab, "public interface ", IHierarchy, "<TKey>"))
+                .AppendLine(Concat(TwoTabs, WhereUnmanaged("TKey")))
+                .AppendLine(Concat(Tab, CurlyBraceOpen))
+                .AppendLine(Concat(Tab, CurlyBraceClose))
+                .AppendLine();
+            for (byte labels = 0; labels <= MaxSameElements; labels++)
+                for (byte hierarchies = 0; hierarchies <= MaxSameElements; hierarchies++)
+                {
+                    if (labels + hierarchies == 0 || (labels == 0 && hierarchies == 1)) continue;
+                    stringBuilder
+                        .AppendLine(Concat(Tab, "public interface ", IHierarchy, "<TKey,"))
+                        .AppendGenericLabelTypes(labels, hierarchies);
+                    stringBuilder
+                        .AppendGenericHierarchyTypes(hierarchies)
+                        .AppendLine(Concat(TwoTabs, WhereUnmanaged("TKey")))
+                        .AppendGenericLabelConstraints(labels)
+                        .AppendGenericHierarchyConstraints(hierarchies)
+                        .AppendLine(Concat(Tab, CurlyBraceOpen))
+                        .AppendLine(Concat(Tab, CurlyBraceClose));
+                    if (labels != MaxSameElements || hierarchies != MaxSameElements)
+                        stringBuilder.AppendLine();
+                }
+            return stringBuilder;
+        }
+
+        private static StringBuilder AppendGenericHierarchyConstraints(this StringBuilder stringBuilder, in byte quantity)
+        {
+            for (byte position = 1; position <= quantity; position++)
+                stringBuilder
+                    .AppendLine(Concat(TwoTabs, WhereUnmanaged(Concat(THierarchyKey, position))))
+                    .AppendLine(Concat(TwoTabs, Concat(WhereClause(Concat(THierarchy, position)), "IHierarchy<THierarchyKey", position, CloseAngleBrace)));
+            return stringBuilder;
+        }
+
+        private static StringBuilder AppendGenericHierarchyTypes(this StringBuilder stringBuilder, in byte quantity)
+        {
+            for (byte label = 1; label <= quantity; label++)
+                stringBuilder.AppendLine(Concat(TwoTabs, GetHierarchyGenerics(label),
+                    CommaOrEmpty(in label, in quantity),
+                    CloseOrEmpty(in label, in quantity)));
+            return stringBuilder;
+            string GetHierarchyGenerics(in byte position) => Concat(THierarchy, position, CommaSpace, THierarchyKey, position);
+        }
+
+        private static StringBuilder AppendGenericLabelConstraints(this StringBuilder stringBuilder, in byte quantity)
+        {
+            for (byte label = 1; label <= quantity; label++)
+                stringBuilder
+                    .AppendLine(Concat(TwoTabs, WhereUnmanaged(Concat(TDatumKey, label))))
+                    .AppendLine(Concat(TwoTabs, WhereUnmanaged(Concat(TDatumLabelKey, label))))
+                    .AppendLine(Concat(TwoTabs, Concat(WhereClause(Concat(TDatum, label)), "IDatum<TDatumKey", label, CommaSpace, TDatumValue, label, CloseAngleBrace)))
+                    .AppendLine(Concat(TwoTabs, Concat(WhereClause(Concat(TDatumLabel, label)), "IDatumLabel<TDatumLabelKey", label, CommaSpace, TDatum, label, CommaSpace, TDatumKey, label, CommaSpace, TDatumValue, label, CloseAngleBrace)));
+            return stringBuilder;
+        }
+
+        private static StringBuilder AppendGenericLabelTypes(this StringBuilder stringBuilder, in byte quantity, in byte hierarchies)
+        {
+            var noHierarchies = hierarchies == byte.MinValue;
+            for (byte label = 1; label <= quantity; label++)
+                stringBuilder.AppendLine(Concat(TwoTabs, GetLabelGenerics(label),
+                    CommaOrEmpty(in label, in quantity, in noHierarchies),
+                    CloseOrEmpty(in label, in quantity, in noHierarchies)));
+            return stringBuilder;
+            string GetLabelGenerics(in byte position) => Concat(TDatumLabel, position, CommaSpace, TDatumLabelKey, position, CommaSpace, TDatum, position, CommaSpace, TDatumKey, position, CommaSpace, TDatumValue, position);
+        }
+
+        private static string CloseOrEmpty(in byte position, in byte last, in bool zeroHierarchies = true)
+            => AppendCloseAngleBrace(in position, in last, in zeroHierarchies) ? ">\r\n\t\t: IHierarchy<TKey>" : Empty;
+
+        private static string CommaOrEmpty(in byte position, in byte last, in bool zeroHierarchies = true)
+            => AppendComma(in position, in last, in zeroHierarchies) ? Comma : Empty;
 
         private static string GetFileContent()
             => GetFileContentPrefix()
@@ -25,22 +117,15 @@ namespace Jigs
                 .AppendLine(CurlyBraceClose)
                 .ToString();
 
-        private static StringBuilder AppendInterfaceGenericOverloads(this StringBuilder stringBuilder)
-        {
-            for (byte labels = 0; labels <= MaxSameElements; labels++)
-                for (byte hierarchies = 0; hierarchies <= MaxSameElements; hierarchies++)
-                {
-                    if (labels + hierarchies == 0) continue;
-                    stringBuilder.AppendLine(Concat(Tab, "public interface ", IHierarchy))
-                        .AppendLine(Concat(Tab, CurlyBraceOpen))
-                        .AppendLine(Concat(Tab, CurlyBraceClose));
-                }
-            return stringBuilder;
-        }
-
         private static StringBuilder GetFileContentPrefix()
             => new StringBuilder()
                 .AppendLine(Concat("namespace ", Shared, ".", Elements))
                 .AppendLine(CurlyBraceOpen);
+
+        private static string WhereClause(in string name)
+            => Concat("where ", name, " : ");
+
+        private static string WhereUnmanaged(in string name)
+            => Concat(WhereClause(name), "unmanaged");
     }
 }
--- Shared/Elements/IHierarchy.cs
+++ Shared/Elements/IHierarchy.cs
@@ -1,6 +1,280 @@
 namespace Shared.Elements
 {
-	public interface IHierarchy
+	public interface IHierarchy<TKey>
+		where TKey : unmanaged
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2,
+		THierarchy3, THierarchyKey3>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+		where THierarchyKey3 : unmanaged
+		where THierarchy3 : IHierarchy<THierarchyKey3>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		THierarchy1, THierarchyKey1>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2,
+		THierarchy3, THierarchyKey3>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+		where THierarchyKey3 : unmanaged
+		where THierarchy3 : IHierarchy<THierarchyKey3>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		THierarchy1, THierarchyKey1>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2,
+		THierarchy3, THierarchyKey3>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+		where THierarchyKey3 : unmanaged
+		where THierarchy3 : IHierarchy<THierarchyKey3>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		TDatumLabel3, TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where TDatumKey3 : unmanaged
+		where TDatumLabelKey3 : unmanaged
+		where TDatum3 : IDatum<TDatumKey3, TDatumValue3>
+		where TDatumLabel3 : IDatumLabel<TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		TDatumLabel3, TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3,
+		THierarchy1, THierarchyKey1>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where TDatumKey3 : unmanaged
+		where TDatumLabelKey3 : unmanaged
+		where TDatum3 : IDatum<TDatumKey3, TDatumValue3>
+		where TDatumLabel3 : IDatumLabel<TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		TDatumLabel3, TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where TDatumKey3 : unmanaged
+		where TDatumLabelKey3 : unmanaged
+		where TDatum3 : IDatum<TDatumKey3, TDatumValue3>
+		where TDatumLabel3 : IDatumLabel<TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+	{
+	}
+
+	public interface IHierarchy<TKey,
+		TDatumLabel1, TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1,
+		TDatumLabel2, TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2,
+		TDatumLabel3, TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3,
+		THierarchy1, THierarchyKey1,
+		THierarchy2, THierarchyKey2,
+		THierarchy3, THierarchyKey3>
+		: IHierarchy<TKey>
+		where TKey : unmanaged
+		where TDatumKey1 : unmanaged
+		where TDatumLabelKey1 : unmanaged
+		where TDatum1 : IDatum<TDatumKey1, TDatumValue1>
+		where TDatumLabel1 : IDatumLabel<TDatumLabelKey1, TDatum1, TDatumKey1, TDatumValue1>
+		where TDatumKey2 : unmanaged
+		where TDatumLabelKey2 : unmanaged
+		where TDatum2 : IDatum<TDatumKey2, TDatumValue2>
+		where TDatumLabel2 : IDatumLabel<TDatumLabelKey2, TDatum2, TDatumKey2, TDatumValue2>
+		where TDatumKey3 : unmanaged
+		where TDatumLabelKey3 : unmanaged
+		where TDatum3 : IDatum<TDatumKey3, TDatumValue3>
+		where TDatumLabel3 : IDatumLabel<TDatumLabelKey3, TDatum3, TDatumKey3, TDatumValue3>
+		where THierarchyKey1 : unmanaged
+		where THierarchy1 : IHierarchy<THierarchyKey1>
+		where THierarchyKey2 : unmanaged
+		where THierarchy2 : IHierarchy<THierarchyKey2>
+		where THierarchyKey3 : unmanaged
+		where THierarchy3 : IHierarchy<THierarchyKey3>
 	{
 	}
 }
