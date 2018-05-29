0698bb8bc6cd983aa64319a4c13a6715195fe7c1
Christopher Dorst <chris@dorst.email>
2018-05-29 11:05:35 -0500

    Add IHierarchyAttribute interface

---
+++ Shared/Elements/IHierarchyAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Elements
+{
+    public interface IHierarchyAttribute<THierarchyKey, TAttribute, TAttributeKey> : IElement<THierarchyKey>
+        where TAttributeKey : unmanaged
+        where THierarchyKey : unmanaged
+        where TAttribute : IHierarchy<TAttributeKey>
+    {
+        TAttribute Attribute { get; }
+        TAttributeKey AttributeId { get; }
+    }
+}
