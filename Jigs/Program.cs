8384b557cc81527f8e93c04718fac569e5d5a4c1
Christopher Dorst <chris@dorst.email>
2018-05-29 06:56:00 -0500

    Add solution path readonly field

--- Jigs/Program.cs
+++ Jigs/Program.cs
@@ -1,9 +1,13 @@
-﻿using static Jigs.SharedElementHierarchy;
+﻿using System;
+using System.IO;
+using static Jigs.SharedElementHierarchy;
 
 namespace Jigs
 {
     public static class Program
     {
+        internal static readonly string SolutionPath = new DirectoryInfo(Environment.CurrentDirectory).Parent.FullName;
+
         public static void Main(string[] args) => WriteIHierarchyCsFile();
     }
 }
