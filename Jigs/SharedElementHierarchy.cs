978afa97e5d4efa35cec8950a82062a2e7a40d34
Christopher Dorst <chris@dorst.email>
2018-05-29 07:03:31 -0500

    Refactor file-contents responsibility

--- Jigs/SharedElementHierarchy.cs
+++ Jigs/SharedElementHierarchy.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using System.IO;
 
 namespace Jigs
 {
     public static class SharedElementHierarchy
     {
-        public static void WriteIHierarchyCsFile() => throw new NotImplementedException();
+        private static readonly string _path = Path.Combine(Program.SolutionPath, "Shared", "Elements", "IHierarchy.cs");
+
+        public static void WriteIHierarchyCsFile() => File.WriteAllText(_path, GetIHierarcyCs());
+
+        private static string GetIHierarcyCs() => string.Empty;
     }
 }
