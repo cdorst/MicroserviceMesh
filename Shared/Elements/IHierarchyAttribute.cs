49414e6a9ed5ce50af807df8ad0768bfc4996a01
Christopher Dorst <chris@dorst.email>
2018-05-29 12:03:15 -0500

    Add label hierarchy attribute interface

--- Shared/Elements/IHierarchyAttribute.cs
+++ Shared/Elements/IHierarchyAttribute.cs
@@ -1,10 +1,24 @@
 ﻿namespace Shared.Elements
 {
-    public interface IHierarchyAttribute<TElement, THierarchyKey, TAttribute, TAttributeKey> : IElement<TElement, THierarchyKey>
+    public interface IHierarchyAttribute<TElement, THierarchyKey, TAttribute, TAttributeKey> 
+        : IElement<TElement, THierarchyKey>
         where THierarchyKey : unmanaged
         where TElement : IElement<TElement, THierarchyKey>
         where TAttributeKey : unmanaged
-        where TAttribute : IHierarchy<TAttributeKey>
+        where TAttribute : IHierarchy<TAttribute, TAttributeKey>
+    {
+        TAttribute Attribute { get; }
+        TAttributeKey AttributeId { get; }
+    }
+
+    public interface IHierarchyAttribute<TElement, THierarchyKey, TAttribute, TAttributeKey, TAttributeDatum, TAttributeDatumKey, TAttributeDatumValue> 
+        : IElement<TElement, THierarchyKey>
+        where THierarchyKey : unmanaged
+        where TElement : IElement<TElement, THierarchyKey>
+        where TAttributeKey : unmanaged
+        where TAttributeDatumKey : unmanaged
+        where TAttributeDatum : IDatum<TAttributeDatum, TAttributeDatumKey, TAttributeDatumValue>
+        where TAttribute : IDatumLabel<TAttribute, TAttributeKey, TAttributeDatum, TAttributeDatumKey, TAttributeDatumValue>
     {
         TAttribute Attribute { get; }
         TAttributeKey AttributeId { get; }
