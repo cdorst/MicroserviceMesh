73f4392462a852161532fd55d65fd493106a83d8
Christopher Dorst <chris@dorst.email>
2018-05-29 07:25:26 -0500

    Add iterator (incomplete) and refactor

--- Jigs/SharedElementHierarchy.cs
+++ Jigs/SharedElementHierarchy.cs
@@ -12,6 +12,7 @@ namespace Jigs
         private const string CurlyBraceOpen = "{";
         private const string Elements = nameof(Elements);
         private const string IHierarchy = nameof(IHierarchy);
+        private const byte MaxSameElements = 3;
         private const string Shared = nameof(Shared);
         private const string Tab = "\t";
         private static readonly string _path = Combine(SolutionPath, Shared, Elements, Concat(IHierarchy, ".cs"));
@@ -19,13 +20,27 @@ namespace Jigs
         public static void WriteFile() => WriteAllText(_path, GetFileContent());
 
         private static string GetFileContent()
-            => new StringBuilder()
-                .AppendLine(Concat("namespace ", Shared, ".", Elements))
-                .AppendLine(CurlyBraceOpen)
-                .AppendLine(Concat(Tab, "public interface ", IHierarchy))
-                .AppendLine(Concat(Tab, CurlyBraceOpen))
-                .AppendLine(Concat(Tab, CurlyBraceClose))
+            => GetFileContentPrefix()
+                .AppendInterfaceGenericOverloads()
                 .AppendLine(CurlyBraceClose)
                 .ToString();
+
+        private static StringBuilder AppendInterfaceGenericOverloads(this StringBuilder stringBuilder)
+        {
+            for (byte labels = 0; labels <= MaxSameElements; labels++)
+                for (byte hierarchies = 0; hierarchies <= MaxSameElements; hierarchies++)
+                {
+                    if (labels + hierarchies == 0) continue;
+                    stringBuilder.AppendLine(Concat(Tab, "public interface ", IHierarchy))
+                        .AppendLine(Concat(Tab, CurlyBraceOpen))
+                        .AppendLine(Concat(Tab, CurlyBraceClose));
+                }
+            return stringBuilder;
+        }
+
+        private static StringBuilder GetFileContentPrefix()
+            => new StringBuilder()
+                .AppendLine(Concat("namespace ", Shared, ".", Elements))
+                .AppendLine(CurlyBraceOpen);
     }
 }
