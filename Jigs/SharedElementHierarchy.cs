b70159bdde8a4dbafde9a1d41f4d1bf4c747e514
Christopher Dorst <chris@dorst.email>
2018-05-29 07:13:49 -0500

    Reuse strings

--- Jigs/SharedElementHierarchy.cs
+++ Jigs/SharedElementHierarchy.cs
@@ -2,23 +2,30 @@
 using static Jigs.Program;
 using static System.IO.File;
 using static System.IO.Path;
+using static System.String;
 
 namespace Jigs
 {
     public static class SharedElementHierarchy
     {
-        private static readonly string _path = Combine(SolutionPath, "Shared", "Elements", "IHierarchy.cs");
+        private const string CurlyBraceClose = "}";
+        private const string CurlyBraceOpen = "{";
+        private const string Elements = nameof(Elements);
+        private const string IHierarchy = nameof(IHierarchy);
+        private const string Shared = nameof(Shared);
+        private const string Tab = "\t";
+        private static readonly string _path = Combine(SolutionPath, Shared, Elements, Concat(IHierarchy, ".cs"));
 
         public static void WriteFile() => WriteAllText(_path, GetFileContent());
 
         private static string GetFileContent()
             => new StringBuilder()
-                .AppendLine("namespace Shared.Elements")
-                .AppendLine("{")
-                .AppendLine("\tpublic interface IHierarchy")
-                .AppendLine("\t{")
-                .AppendLine("\t}")
-                .AppendLine("}")
+                .AppendLine(Concat("namespace ", Shared, ".", Elements))
+                .AppendLine(CurlyBraceOpen)
+                .AppendLine(Concat(Tab, "public interface ", IHierarchy))
+                .AppendLine(Concat(Tab, CurlyBraceOpen))
+                .AppendLine(Concat(Tab, CurlyBraceClose))
+                .AppendLine(CurlyBraceClose)
                 .ToString();
     }
 }
