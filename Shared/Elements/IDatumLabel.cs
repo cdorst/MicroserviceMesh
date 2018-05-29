c2f0ab6a6bd33c5db9cc731dff7a3c217ab0385a
Christopher Dorst <chris@dorst.email>
2018-05-29 06:28:27 -0500

    Add interface for data label types

---
+++ Shared/Elements/IDatumLabel.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Elements
+{
+    public interface IDatumLabel<TKey, TDatum, TDatumKey, TDatumValue> : IElement<TKey>
+        where TKey : unmanaged
+        where TDatumKey : unmanaged
+        where TDatum : IDatum<TDatumKey, TDatumValue>
+    {
+        IDatum<TDatumKey, TDatumValue> Datum { get; }
+        TDatumKey DatumId { get; }
+    }
+}
