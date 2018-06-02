e2ca7cfd1541ec30e169307f30078b3d934d5602
Christopher Dorst <chris@dorst.email>
2018-06-02 14:23:51 -0500

    Add DeclarationRoot type

---
+++ Declaration.Generator/DeclarationRoot.cs
@@ -0,0 +1,13 @@
+﻿using Declaration.Generator.Types;
+using System.Collections.Generic;
+
+namespace Declaration.Generator
+{
+    public class DeclarationRoot
+    {
+        public DeclarationRoot(in IEnumerable<Layer> layers)
+        {
+
+        }
+    }
+}
