7485bf1d430ee7e29d371b58653098aa3dd33f24
Christopher Dorst <chris@dorst.email>
2018-06-03 05:59:13 -0500

    Simplify field names

--- Declaration.Generator/FileWriter.cs
+++ Declaration.Generator/FileWriter.cs
@@ -8,21 +8,28 @@ namespace Declaration.Generator
 {
     internal static class FileWriter
     {
-        private static readonly string[] _fileExtension = { ".cs" };
-        private static readonly string[] _rootDirectory = { new DirectoryInfo(CurrentDirectory).Parent.FullName, "Declaration", "Code" };
+        private const string _extension = ".cs";
+        private static readonly string[] _directory = { new DirectoryInfo(CurrentDirectory).Parent.FullName, "Declaration", "Code" };
 
         public static void WriteFile(in string content, params string[] pathParts)
              => WriteAllText(
                  GetFilePath(in pathParts),
                  content);
 
-        private static string GetFilePath(in string[] pathParts)
-            => Combine(GetFilePathArray(in pathParts));
+        private static string GetFilePath(in string[] parts)
+            => Combine(GetFilePathArray(in parts));
 
-        private static string[] GetFilePathArray(in string[] pathParts)
-            => _rootDirectory
-                .Concat(pathParts)
-                .Concat(_fileExtension)
+        private static string[] GetFilePathArray(in string[] parts)
+            => _directory
+                .Concat(GetPathPartsWithFileExtension(parts))
                 .ToArray();
+
+        private static string[] GetPathPartsWithFileExtension(string[] parts)
+        {
+            var last = parts.Length - 1;
+            if (parts[last].EndsWith(_extension)) return parts;
+            parts[last] = string.Concat(parts[last], _extension);
+            return parts;
+        }
     }
 }
