ed0b560c06dabf773050f1bbcf39f2281f637a1d
Christopher Dorst <chris@dorst.email>
2018-06-03 06:56:59 -0500

    Add DeclarationCodeGenerator type

---
+++ Declaration.Generator/DeclarationCodeGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Declaration.Generator.Types;
+using System;
+
+namespace Declaration.Generator
+{
+    internal static class DeclarationCodeGenerator
+    {
+        public static string GenerateCode(in Layer layer) => throw new NotImplementedException();
+    }
+}
