661b885304b07dd544c236904c244b70d6a0da8a
Christopher Dorst <chris@dorst.email>
2018-06-03 11:49:26 -0500

    Refactor copyright-value responsibility

--- Declaration.Generator/DeclarationCodeGenerator.cs
+++ Declaration.Generator/DeclarationCodeGenerator.cs
@@ -1,6 +1,6 @@
 ﻿using Declaration.Generator.Types;
+using static Declaration.Generator.DeclarationCodeCopyrightFactory;
 using static DevOps.Primitives.SourceGraph.Helpers.DotNetCore.Common.Files.CSharpCode;
-using static System.Environment;
 using static System.String;
 
 namespace Declaration.Generator
@@ -8,9 +8,6 @@ namespace Declaration.Generator
     internal static class DeclarationCodeGenerator
     {
         private const string ProjectName = nameof(Declaration);
-        private static readonly string _copyrightEnvironmentValue = GetEnvironmentVariable("CodeGenerator_Copyright");
-        private static readonly string _copyrightValue = Concat("Copyright © ",
-            IsNullOrWhiteSpace(_copyrightEnvironmentValue) ? "Christopher Dorst" : _copyrightEnvironmentValue);
 
         public static DeclarationFile GenerateCode(in Layer layer)
         {
@@ -21,7 +18,7 @@ namespace Declaration.Generator
         }
 
         private static string GetTypeDeclaration(in Layer layer, in string[] pathParts)
-            => CSharpStaticClass(_copyrightValue, ProjectName, layer.Name, GetTypeNamespace(in pathParts)).Content.Value; // TODO add type members
+            => CSharpStaticClass(CopyrightValue, ProjectName, layer.Name, GetTypeNamespace(in pathParts)).Content.Value; // TODO add type members
 
         private static string GetTypeNamespace(in string[] pathParts)
             => Concat(ProjectName, ".Code", pathParts[0], ".", pathParts[1]);
