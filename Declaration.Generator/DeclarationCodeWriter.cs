f64a6c800f9f4286c8d6a8e99123c5dc0416718a
Christopher Dorst <chris@dorst.email>
2018-06-03 06:47:31 -0500

    Refactor DeclarationCodeWriter responsibility; separates ConsoleApp-startup & business-logic concerns

---
+++ Declaration.Generator/DeclarationCodeWriter.cs
@@ -0,0 +1,21 @@
+﻿using Declaration.Generator.Types;
+using System.Collections.Generic;
+using static Declaration.Generator.DeclarationDeserializer;
+using static Declaration.Generator.FileWriter;
+
+namespace Declaration.Generator
+{
+    internal static class DeclarationCodeWriter
+    {
+        public static void WriteDeclarationCode() => WriteFiles(DeserializeDeclaration());
+
+        private static void WriteFiles(in IEnumerable<Layer> layers)
+        {
+            foreach (var layer in layers)
+            {
+                var declaration = layer.AsDeclarationFile();
+                WriteFile(in declaration.Contents, declaration.PathParts);
+            }
+        }
+    }
+}
--- Declaration.Generator/Program.cs
+++ Declaration.Generator/Program.cs
@@ -1,22 +1,9 @@
-﻿using Declaration.Generator.Types;
-using System.Collections.Generic;
-using static Declaration.Generator.DeclarationDeserializer;
-using static Declaration.Generator.FileWriter;
+﻿using static Declaration.Generator.DeclarationCodeWriter;
 
 namespace Declaration.Generator
 {
     public static class Program
     {
-        public static void Main(string[] args = default)
-            => WriteFiles(DeserializeDeclaration());
-
-        private static void WriteFiles(in IEnumerable<Layer> layers)
-        {
-            foreach (var layer in layers)
-            {
-                var declaration = layer.AsDeclarationFile();
-                WriteFile(in declaration.Contents, declaration.PathParts);
-            }
-        }
+        public static void Main(string[] args = default) => WriteDeclarationCode();
     }
 }
