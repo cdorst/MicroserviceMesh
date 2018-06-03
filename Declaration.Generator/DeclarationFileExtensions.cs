c506bef627448da847a5d9bf290ee2186f582dc2
Christopher Dorst <chris@dorst.email>
2018-06-03 06:34:56 -0500

    Add Layer.AsDeclarationFile extension

---
+++ Declaration.Generator/DeclarationFileExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Declaration.Generator.Types;
+using System;
+
+namespace Declaration.Generator
+{
+    public static class DeclarationFileExtensions
+    {
+        public static DeclarationFile AsDeclarationFile(this Layer layer)
+            => throw new NotImplementedException();
+    }
+}
