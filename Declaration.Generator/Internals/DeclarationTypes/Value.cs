c1b4f31a6f1df52984ffdbb223ae5e4d9927fd15
Christopher Dorst <chris@dorst.email>
2018-06-04 15:55:43 -0500

    Remove keySize parameter

--- Declaration.Generator/Internals/DeclarationTypes/Value.cs
+++ Declaration.Generator/Internals/DeclarationTypes/Value.cs
@@ -5,17 +5,17 @@ namespace Declaration.Generator.Internals.DeclarationTypes
     public class Value : NamedTypedKeyedEntity
     {
         public Value() { }
-        public Value(in string type, in string @namespace = default, in byte? keySize = default)
-            : base(in type, in @namespace, in keySize)
+        public Value(in string type, in string @namespace = default)
+            : base(in type, in @namespace)
         {
         }
-        public Value(in string type, in Attributes attributes, in string @namespace = default, in byte? keySize = default)
-            : this(in type, in @namespace, in keySize)
+        public Value(in string type, in Attributes attributes, in string @namespace = default)
+            : this(in type, in @namespace)
         {
             Attributes = attributes;
         }
-        public Value(in string type, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string @namespace = default, in byte? keySize = default)
-            : this(in type, in attributes, in @namespace, in keySize)
+        public Value(in string type, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string @namespace = default)
+            : this(in type, in attributes, in @namespace)
         {
             ShadowAlternateKey = alternateKey;
         }
