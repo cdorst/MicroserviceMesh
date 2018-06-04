b064b69eb9b6c04e23c4c8f25fe80699d659a0fd
Christopher Dorst <chris@dorst.email>
2018-06-04 11:12:06 -0500

    Implement generating blank classes for Layer & Entity delcaration-file types

--- Declaration.Generator/Internals/DeclarationCodeGenerator.cs
+++ Declaration.Generator/Internals/DeclarationCodeGenerator.cs
@@ -9,38 +9,44 @@ namespace Declaration.Generator.Internals
 {
     internal static class DeclarationCodeGenerator
     {
-        private const string Entity = nameof(Entity);
-        private const string Layers = nameof(Layers);
+        private const string Entities = nameof(Entities);
+        private const string Layer = nameof(Layer);
         private const string ProjectName = nameof(Declaration);
+        private const string Underscore = "_";
         private static readonly string _namespacePrefix = Concat(ProjectName, ".Layers.");
 
         public static IEnumerable<DeclarationFile> GenerateCode(Layer layer, IConfigurationRoot configuration)
         {
-            var path = Concat(layer.BlockName, "_", layer.Name);
+            var path = Concat("Block", Underscore, layer.BlockName, Underscore, layer.Name);
 
             // Declare file: Declaration/Layers/{path}/Layer.cs
             yield return GetLayer(in layer, in path, in configuration);
 
             foreach (var entity in layer.Entities)
                 // Declare file: Declaration/Layers/{path}/Entities/{name}.cs
-                yield return GetEntity(in layer, in path, in configuration);
+                yield return GetEntity(in entity, in layer, in path, in configuration);
         }
 
-        private static DeclarationFile GetEntity(in Layer layer, in string path, in IConfigurationRoot configuration)
-            => new DeclarationFile(
-                GetTypeDeclaration(in layer, GetTypeNamespace(in path), in configuration),
-                path);
+        private static DeclarationFile GetEntity(in Entity entity, in Layer layer, in string path, in IConfigurationRoot configuration)
+        {
+            entity.BlockName = layer.BlockName;
+            entity.LayerName = layer.Name;
+            var name = entity.GetTypeName().Type;
+            return new DeclarationFile(
+                GetTypeDeclaration(name, GetTypeNamespace(in path), in configuration),
+                path, Entities, name);
+        }
 
         private static DeclarationFile GetLayer(in Layer layer, in string path, in IConfigurationRoot configuration)
             => new DeclarationFile(
-                GetTypeDeclaration(in layer, GetTypeNamespace(in path), in configuration),
-                path);
+                GetTypeDeclaration(Layer, GetTypeNamespace(in path), in configuration),
+                path, Layer);
 
-        private static string GetTypeDeclaration(in Layer layer, in string @namespace, in IConfigurationRoot configuration)
+        private static string GetTypeDeclaration(in string typeName, in string @namespace, in IConfigurationRoot configuration)
             => CSharpStaticClass(
                 copyright:      CopyrightValue(in configuration),
                 projectName:    ProjectName,
-                typeName:       layer.Name,
+                typeName:       typeName,
                 @namespace:     @namespace
                 ).Content.Value; // TODO add type members
 
