a5ecbbb7c1d9f8a32f24a9bf638ef11469df7254
Christopher Dorst <chris@dorst.email>
2018-06-04 10:31:39 -0500

    Revise GetTypeNamespace implementation

--- Declaration.Generator/Internals/DeclarationCodeGenerator.cs
+++ Declaration.Generator/Internals/DeclarationCodeGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Linq;
 using static Declaration.Generator.Internals.DeclarationCodeCopyrightFactory;
 using static DevOps.Primitives.SourceGraph.Helpers.DotNetCore.Common.Files.CSharpCode;
 using static System.String;
@@ -9,27 +10,32 @@ namespace Declaration.Generator.Internals
 {
     internal static class DeclarationCodeGenerator
     {
+        private const string Entity = nameof(Entity);
+        private const string Layers = nameof(Layers);
         private const string ProjectName = nameof(Declaration);
+        private static readonly string _namespacePrefix = Concat(ProjectName, ".Layers.");
 
         public static IEnumerable<DeclarationFile> GenerateCode(Layer layer, IConfigurationRoot configuration)
         {
-            var pathParts = layer.GetPathParts();
+            var path = Concat(layer.BlockName, "_", layer.Name);
 
             // yield return /Layers/{name}/Layer.cs
             yield return new DeclarationFile(
-                GetTypeDeclaration(in layer, in pathParts, in configuration),
-                pathParts);
+                GetTypeDeclaration(in layer, GetTypeNamespace(in path), in configuration),
+                path);
         }
 
-        private static string GetTypeDeclaration(in Layer layer, in string[] pathParts, in IConfigurationRoot configuration)
+        private static string GetTypeDeclaration(in Layer layer, in string @namespace, in IConfigurationRoot configuration)
             => CSharpStaticClass(
                 copyright:      CopyrightValue(in configuration),
                 projectName:    ProjectName,
                 typeName:       layer.Name,
-                @namespace:     GetTypeNamespace(in pathParts)
+                @namespace:     @namespace
                 ).Content.Value; // TODO add type members
 
-        private static string GetTypeNamespace(in string[] pathParts)
-            => Concat(ProjectName, ".Code", pathParts[0], ".", pathParts[1]);
+        private static string GetTypeNamespace(in string folder, in string subfolder = default)
+            => IsNullOrWhiteSpace(subfolder)
+                ? Concat(_namespacePrefix, folder)
+                : Concat(_namespacePrefix, folder, ".", subfolder);
     }
 }
