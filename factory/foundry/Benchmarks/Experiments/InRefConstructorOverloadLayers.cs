f3e449c966349caaf555a4a860c3b52ff2685d39
Christopher Dorst <chris@dorst.email>
2018-06-04 18:05:46 -0500

    Introduce 1000-iteration for-loop to reduce SimpleJob-iteration job-result variability

--- factory/foundry/Benchmarks/Experiments/InRefConstructorOverloadLayers.cs
+++ factory/foundry/Benchmarks/Experiments/InRefConstructorOverloadLayers.cs
@@ -13,6 +13,8 @@ namespace Benchmarks.Experiments
     {
         private static class Constants
         {
+            public const int Iterations = 1000;
+
             public static readonly string Block = nameof(Block);
             public static readonly string Layer = nameof(Layer);
             public static readonly string Type = nameof(Type);
@@ -21,12 +23,24 @@ namespace Benchmarks.Experiments
         }
 
         [Benchmark]
-        public WithInKeyword WithInKeyword() => new WithInKeyword(Constants.Block, Constants.Layer, Constants.Type, Constants.ValueType, Constants.ValueNamespace);
+        public void WithInKeyword()
+        {
+            for (int i = 0; i < Constants.Iterations; i++)
+                new WithInKeyword(Constants.Block, Constants.Layer, Constants.Type, Constants.ValueType, Constants.ValueNamespace);
+        }
 
         [Benchmark]
-        public WithInKeyword WithInKeyword_InRefInvocation() => new WithInKeyword(in Constants.Block, in Constants.Layer, in Constants.Type, in Constants.ValueType, in Constants.ValueNamespace);
+        public void WithInKeyword_InRefInvocation()
+        {
+            for (int i = 0; i < Constants.Iterations; i++)
+                new WithInKeyword(in Constants.Block, in Constants.Layer, in Constants.Type, in Constants.ValueType, in Constants.ValueNamespace);
+        }
 
         [Benchmark]
-        public WithoutInKeyword WithoutInKeyword() => new WithoutInKeyword(Constants.Block, Constants.Layer, Constants.Type, Constants.ValueType, Constants.ValueNamespace);
+        public void WithoutInKeyword()
+        {
+            for (int i = 0; i < Constants.Iterations; i++)
+                new WithoutInKeyword(Constants.Block, Constants.Layer, Constants.Type, Constants.ValueType, Constants.ValueNamespace);
+        }
     }
 }
