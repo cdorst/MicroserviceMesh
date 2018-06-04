ac04fe03c35eb32c1d5677e961c5f44e36ca5899
Christopher Dorst <chris@dorst.email>
2018-06-04 10:00:31 -0500

    Refactor configuration responsibility

---
+++ Declaration.Generator/Internals/ConfigurationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using static System.Environment;
+
+namespace Declaration.Generator.Internals
+{
+    internal static class ConfigurationFactory
+    {
+        public static readonly IConfigurationRoot Configuration
+            = new ConfigurationBuilder()
+                .SetBasePath(CurrentDirectory)
+                .AddJsonFile("configuration.json")
+                .Build();
+    }
+}
--- Declaration.Generator/Program.cs
+++ Declaration.Generator/Program.cs
@@ -1,17 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using static Declaration.Generator.Internals.ConfigurationFactory;
 using static Declaration.Generator.Internals.DeclarationCodeWriter;
-using static System.Environment;
 
 namespace Declaration.Generator
 {
     public static class Program
     {
-        private static readonly IConfigurationRoot Configuration
-            = new ConfigurationBuilder()
-                .SetBasePath(CurrentDirectory)
-                .AddJsonFile("configuration.json")
-                .Build();
-
         public static void Main(string[] args = default)
             => WriteDeclarationCode(in Configuration);
     }
