dd1f3faf05348cff34f2d955e2cb620613bfd268
Christopher Dorst <chris@dorst.email>
2018-06-04 11:12:28 -0500

    Generate empty declaration classes

---
+++ Declaration/Layers/Block_00000_Primitives_00001_Integers/Entities/Int16Reference.cs
@@ -0,0 +1,9 @@
+// Copyright © 2018 Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
+namespace Declaration.Layers.Block_00000_Primitives_00001_Integers
+{
+    public static class Int16Reference
+    {
+    }
+}
