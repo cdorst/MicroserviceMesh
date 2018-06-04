cb429ed888a90d5013948835a49e39e6812801bd
Christopher Dorst <chris@dorst.email>
2018-06-04 16:40:25 -0500

    Add Entities constructor helper

---
+++ Declaration.Generator/Internals/DeclarationTypes/Helpers/Entities.cs
@@ -0,0 +1,8 @@
+﻿namespace Declaration.Generator.Internals.DeclarationTypes.Helpers
+{
+    public static class Entities
+    {
+        public static Entity Entity(in string block, in string layer, in string type, in Value value)
+            => new Entity(in block, in layer, in type, in value);
+    }
+}
