e5d88496f0c40f9b054997e1cd7e3ff7534fb772
Christopher Dorst <chris@dorst.email>
2018-06-04 16:43:21 -0500

    Add constructor-helper overloads

--- Declaration.Generator/Internals/DeclarationTypes/Helpers/Entities.cs
+++ Declaration.Generator/Internals/DeclarationTypes/Helpers/Entities.cs
@@ -4,5 +4,14 @@
     {
         public static Entity Entity(in string block, in string layer, in string type, in Value value)
             => new Entity(in block, in layer, in type, in value);
+
+        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in string valueNamespace = default)
+            => Entity(in block, in layer, in type, new Value(in valueType, in valueNamespace));
+
+        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in Attributes attributes, in string valueNamespace = default)
+            => Entity(in block, in layer, in type, new Value(in valueType, in attributes, in valueNamespace));
+
+        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string valueNamespace = default)
+            => Entity(in block, in layer, in type, new Value(in valueType, in alternateKey, in attributes, in valueNamespace));
     }
 }
