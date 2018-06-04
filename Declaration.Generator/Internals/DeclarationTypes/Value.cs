242a6fd2662dc1e0438f6b6745b94f5c4ef5d66e
Christopher Dorst <chris@dorst.email>
2018-06-04 15:46:41 -0500

    Use NamedTypedKeyedEntity base type; add constructors

--- Declaration.Generator/Internals/DeclarationTypes/Value.cs
+++ Declaration.Generator/Internals/DeclarationTypes/Value.cs
@@ -2,8 +2,24 @@
 
 namespace Declaration.Generator.Internals.DeclarationTypes
 {
-    public class Value : DifferentiatedEntity
+    public class Value : NamedTypedKeyedEntity
     {
+        public Value() { }
+        public Value(in string type, in string @namespace = default, in byte? keySize = default)
+            : base(in type, in @namespace, in keySize)
+        {
+        }
+        public Value(in string type, in Attributes attributes, in string @namespace = default, in byte? keySize = default)
+            : this(in type, in @namespace, in keySize)
+        {
+            Attributes = attributes;
+        }
+        public Value(in string type, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string @namespace = default, in byte? keySize = default)
+            : this(in type, in attributes, in @namespace, in keySize)
+        {
+            ShadowAlternateKey = alternateKey;
+        }
+
         public Attributes Attributes { get; set; }
         public ShadowAlternateKey ShadowAlternateKey { get; set; }
     }
