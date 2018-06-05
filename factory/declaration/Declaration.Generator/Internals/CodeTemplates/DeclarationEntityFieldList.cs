5c1d42f9cf25c75cc539f624165f7f439f67c6de
Christopher Dorst <chris@dorst.email>
2018-06-04 20:36:29 -0500

    Pass entity-name parameter & basic (incomplete) Initializer() implementation

--- factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntityFieldList.cs
+++ factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntityFieldList.cs
@@ -2,6 +2,7 @@
 using DevOps.Primitives.CSharp;
 using static DevOps.Primitives.CSharp.Helpers.Common.FieldLists;
 using static DevOps.Primitives.CSharp.Helpers.Common.Fields;
+using static System.String;
 
 namespace Declaration.Generator.Internals.CodeTemplates
 {
@@ -10,11 +11,12 @@ namespace Declaration.Generator.Internals.CodeTemplates
         private const string Comment = "Contains declaration representing this entity type";
         private const string Declaration = nameof(Declaration);
         private const string Entity = nameof(Entity);
+        private const string QuoteCommaSpaceQuote = "\", \"";
 
-        public static FieldList FieldList(in Entity entity, in Layer layer)
-            => Create(PublicStaticReadonly(Declaration, Entity, Comment, initializer: Initializer(in entity, in layer)));
+        public static FieldList FieldList(in Entity entity, in Layer layer, in string typeName)
+            => Create(PublicStaticReadonly(Declaration, Entity, Comment, initializer: Initializer(in entity, in layer, in typeName)));
 
-        private static string Initializer(in Entity entity, in Layer layer)
-            => "default";
+        private static string Initializer(in Entity entity, in Layer layer, in string typeName)
+            => Concat("Entity(\"", entity.BlockName, QuoteCommaSpaceQuote, entity.LayerName, QuoteCommaSpaceQuote, typeName, "\", default)");
     }
 }
