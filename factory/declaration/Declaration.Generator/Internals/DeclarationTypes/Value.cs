c919c4a409ba50069f53a8415df198ea866dcadd
Christopher Dorst <chris@dorst.email>
2018-06-04 19:45:37 -0500

    Add block/layer name properties to Value type

--- factory/declaration/Declaration.Generator/Internals/DeclarationTypes/Value.cs
+++ factory/declaration/Declaration.Generator/Internals/DeclarationTypes/Value.cs
@@ -2,20 +2,20 @@
 
 namespace Declaration.Generator.Internals.DeclarationTypes
 {
-    public class Value : NamedTypedKeyedEntity
+    public class Value : DifferentiatedEntity
     {
         public Value() { }
-        public Value(in string type, in string @namespace = default)
-            : base(in type, in @namespace)
+        public Value(in string type, in string @namespace = default, in string block = default, in string layer = default, in byte? keySize = default)
+            : base(in type, in block, in layer, in @namespace, in keySize)
         {
         }
-        public Value(in string type, in Attributes attributes, in string @namespace = default)
-            : this(in type, in @namespace)
+        public Value(in string type, in Attributes attributes, in string @namespace = default, in string block = default, in string layer = default, in byte? keySize = default)
+            : this(in type, in @namespace, in block, in layer, in keySize)
         {
             Attributes = attributes;
         }
-        public Value(in string type, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string @namespace = default)
-            : this(in type, in attributes, in @namespace)
+        public Value(in string type, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string @namespace = default, in string block = default, in string layer = default, in byte? keySize = default)
+            : this(in type, in attributes, in @namespace, in block, in layer, in keySize)
         {
             ShadowAlternateKey = alternateKey;
         }
