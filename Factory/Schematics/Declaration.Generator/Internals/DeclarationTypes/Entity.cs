bc81c6862b0041edd3e7d61e087dabf2924f9e89
Christopher Dorst <chris@dorst.email>
2018-06-05 10:51:21 -0500

    Add keySize parameter

--- Factory/Schematics/Declaration.Generator/Internals/DeclarationTypes/Entity.cs
+++ Factory/Schematics/Declaration.Generator/Internals/DeclarationTypes/Entity.cs
@@ -8,21 +8,21 @@ namespace Declaration.Generator.Internals.DeclarationTypes
     public class Entity : DifferentiatedEntity
     {
         public Entity() { }
-        public Entity(in string block, in string layer, in string type, in Value value)
-            : base(in block, in layer, in type)
+        public Entity(in string block, in string layer, in string type, in Value value, in byte? keySize = default)
+            : base(in block, in layer, in type, keySize: in keySize)
         {
             Value = value;
         }
-        public Entity(in string block, in string layer, in string type, in string valueType, in string valueNamespace = default)
-            : this(in block, in layer, in type, new Value(in valueType, in valueNamespace))
+        public Entity(in string block, in string layer, in string type, in string valueType, in string valueNamespace = default, in byte? keySize = default)
+            : this(in block, in layer, in type, new Value(in valueType, in valueNamespace), in keySize)
         {
         }
-        public Entity(in string block, in string layer, in string type, in string valueType, in Attributes attributes, in string valueNamespace = default)
-            : this(in block, in layer, in type, new Value(in valueType, in attributes, in valueNamespace))
+        public Entity(in string block, in string layer, in string type, in string valueType, in Attributes attributes, in string valueNamespace = default, in byte? keySize = default)
+            : this(in block, in layer, in type, new Value(in valueType, in attributes, in valueNamespace), in keySize)
         {
         }
-        public Entity(in string block, in string layer, in string type, in string valueType, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string valueNamespace = default)
-            : this(in block, in layer, in type, new Value(in valueType, in alternateKey, in attributes, in valueNamespace))
+        public Entity(in string block, in string layer, in string type, in string valueType, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string valueNamespace = default, in byte? keySize = default)
+            : this(in block, in layer, in type, new Value(in valueType, in alternateKey, in attributes, in valueNamespace), in keySize)
         {
         }
 
--- Factory/Schematics/Declaration.Generator/Internals/DeclarationTypes/Helpers/Entities.cs
+++ Factory/Schematics/Declaration.Generator/Internals/DeclarationTypes/Helpers/Entities.cs
@@ -2,16 +2,16 @@
 {
     public static class Entities
     {
-        public static Entity Entity(in string block, in string layer, in string type, in Value value)
-            => new Entity(in block, in layer, in type, in value);
+        public static Entity Entity(in string block, in string layer, in string type, in Value value, in byte? keySize = default)
+            => new Entity(in block, in layer, in type, in value, in keySize);
 
-        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in string valueNamespace = default)
-            => Entity(in block, in layer, in type, new Value(in valueType, in valueNamespace));
+        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in string valueNamespace = default, in byte? keySize = default)
+            => Entity(in block, in layer, in type, new Value(in valueType, in valueNamespace), in keySize);
 
-        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in Attributes attributes, in string valueNamespace = default)
-            => Entity(in block, in layer, in type, new Value(in valueType, in attributes, in valueNamespace));
+        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in Attributes attributes, in string valueNamespace = default, in byte? keySize = default)
+            => Entity(in block, in layer, in type, new Value(in valueType, in attributes, in valueNamespace), in keySize);
 
-        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string valueNamespace = default)
-            => Entity(in block, in layer, in type, new Value(in valueType, in alternateKey, in attributes, in valueNamespace));
+        public static Entity Entity(in string block, in string layer, in string type, in string valueType, in ShadowAlternateKey alternateKey, in Attributes attributes = default, in string valueNamespace = default, in byte? keySize = default)
+            => Entity(in block, in layer, in type, new Value(in valueType, in alternateKey, in attributes, in valueNamespace), in keySize);
     }
 }
