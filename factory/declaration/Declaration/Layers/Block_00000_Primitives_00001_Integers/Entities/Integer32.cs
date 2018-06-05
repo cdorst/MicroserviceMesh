8549231387fc7fef37c480219859289dba3cab3c
Christopher Dorst <chris@dorst.email>
2018-06-04 20:37:15 -0500

    Generate basic entity declarations

--- factory/declaration/Declaration/Layers/Block_00000_Primitives_00001_Integers/Entities/Integer32.cs
+++ factory/declaration/Declaration/Layers/Block_00000_Primitives_00001_Integers/Entities/Integer32.cs
@@ -1,6 +1,7 @@
 // Copyright © 2018 Christopher Dorst. All rights reserved.
 // Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
 
+using Declaration.Generator.Internals.DeclarationTypes;
 using static Declaration.Generator.Internals.DeclarationTypes.Helpers.Entities;
 
 namespace Declaration.Layers.Block_00000_Primitives_00001_Integers
@@ -9,6 +10,6 @@ namespace Declaration.Layers.Block_00000_Primitives_00001_Integers
     public static class Integer32
     {
         /// <summary>Contains declaration representing this entity type</summary>
-        public static readonly Entity Declaration = default;
+        public static readonly Entity Declaration = Entity("00000_Primitives", "00001_Integers", "Integer32", default);
     }
 }
