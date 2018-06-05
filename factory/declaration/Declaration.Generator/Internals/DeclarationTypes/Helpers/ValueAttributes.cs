2f82707788dad5ce0567a88e757a9e5f9d656911
Christopher Dorst <chris@dorst.email>
2018-06-04 22:11:31 -0500

    Add Attributes arguments

--- factory/declaration/Declaration.Generator/Internals/DeclarationTypes/Helpers/ValueAttributes.cs
+++ factory/declaration/Declaration.Generator/Internals/DeclarationTypes/Helpers/ValueAttributes.cs
@@ -1,7 +1,12 @@
-﻿namespace Declaration.Generator.Internals.DeclarationTypes.Helpers
+﻿using System.Collections.Generic;
+
+namespace Declaration.Generator.Internals.DeclarationTypes.Helpers
 {
     public static class ValueAttributes
     {
-        public static Attributes Attributes() => new Attributes();
+        public static Attributes Attributes(
+            in List<string> listLiterals = default,
+            in string columnType = default)
+            => new Attributes(in listLiterals, in columnType);
     }
 }
