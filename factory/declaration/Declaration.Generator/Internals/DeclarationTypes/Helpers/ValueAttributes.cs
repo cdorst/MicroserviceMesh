e7c97f462e02941106aa7c371d8ad5c059099222
Christopher Dorst <chris@dorst.email>
2018-06-04 22:09:54 -0500

    Add ValueAttributes helpers class

---
+++ factory/declaration/Declaration.Generator/Internals/DeclarationTypes/Helpers/ValueAttributes.cs
@@ -0,0 +1,7 @@
+﻿namespace Declaration.Generator.Internals.DeclarationTypes.Helpers
+{
+    public static class ValueAttributes
+    {
+        public static Attributes Attributes() => new Attributes();
+    }
+}
