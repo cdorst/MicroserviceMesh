ff661d566c7ea8cd2af34989ead2d7934a8bfab7
Christopher Dorst <chris@dorst.email>
2018-06-04 19:54:50 -0500

    Refactor entity FieldList responsibility

--- factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntity.cs
+++ factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntity.cs
@@ -1,19 +1,17 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
 using DevOps.Primitives.CSharp;
 using Microsoft.Extensions.Configuration;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationEntityFieldList;
 using static System.String;
-using Field = DevOps.Primitives.CSharp.Helpers.Common.Fields;
-using Fields = DevOps.Primitives.CSharp.Helpers.Common.FieldLists;
 using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
 
 namespace Declaration.Generator.Internals.CodeTemplates
 {
     internal class DeclarationEntity : DeclarationType
     {
-        private const string Comment = "Contains declaration representing this entity type";
-        private const string Declaration = nameof(Declaration);
         private const string Entities = nameof(Entities);
-        private const string Entity = nameof(Entity);
+
+        private static readonly UsingDirectiveList UsingDirectiveList = Usings.Create("Declaration.Generator.Internals.DeclarationTypes");
 
         public static DeclarationFile GetEntity(in Entity entity, in Layer layer, in string path, in IConfigurationRoot configuration)
         {
@@ -22,11 +20,8 @@ namespace Declaration.Generator.Internals.CodeTemplates
             var name = entity.GetTypeName().Type;
             var comment = Concat("Contains declaration describing ", name, " entity types");
             return new DeclarationFile(
-                GetTypeDeclaration(name, GetTypeNamespace(in path), in configuration, in comment, in UsingDirectiveList, in FieldList),
+                GetTypeDeclaration(name, GetTypeNamespace(in path), in configuration, in comment, in UsingDirectiveList, FieldList(in entity)),
                 path, Entities, name);
         }
-
-        private static readonly FieldList FieldList = Fields.Create(Field.PublicStaticReadonly(Declaration, Entity, Comment, initializer: "default"));
-        private static readonly UsingDirectiveList UsingDirectiveList = Usings.Create("Declaration.Generator.Internals.DeclarationTypes");
     }
 }
---
+++ factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntityFieldList.cs
@@ -0,0 +1,20 @@
+﻿using Declaration.Generator.Internals.DeclarationTypes;
+using DevOps.Primitives.CSharp;
+using Field = DevOps.Primitives.CSharp.Helpers.Common.Fields;
+using Fields = DevOps.Primitives.CSharp.Helpers.Common.FieldLists;
+
+namespace Declaration.Generator.Internals.CodeTemplates
+{
+    internal static class DeclarationEntityFieldList
+    {
+        private const string Comment = "Contains declaration representing this entity type";
+        private const string Declaration = nameof(Declaration);
+        private const string Entity = nameof(Entity);
+
+        public static FieldList FieldList(in Entity entity)
+            => Fields.Create(Field.PublicStaticReadonly(Declaration, Entity, Comment, initializer: Initializer(in entity)));
+
+        private static string Initializer(in Entity entity)
+            => "default";
+    }
+}
