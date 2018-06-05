38f70f338df391007d73fef02b8ab264f382addd
Christopher Dorst <chris@dorst.email>
2018-06-04 20:19:10 -0500

    Refactor entity namespace import responsibility

--- factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntity.cs
+++ factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntity.cs
@@ -1,18 +1,14 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
-using DevOps.Primitives.CSharp;
-using DevOps.Primitives.CSharp.Helpers.Common;
 using Microsoft.Extensions.Configuration;
 using static Declaration.Generator.Internals.CodeTemplates.DeclarationEntityFieldList;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationEntityUsingDirectiveList;
 using static System.String;
-using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
 
 namespace Declaration.Generator.Internals.CodeTemplates
 {
     internal class DeclarationEntity : DeclarationType
     {
         private const string Entities = nameof(Entities);
-
-        private static readonly UsingDirectiveList UsingDirectiveList = Usings.Create(UsingDirectives.UsingStatic("Declaration.Generator.Internals.DeclarationTypes.Helpers.Entities"));
 
         public static DeclarationFile GetEntity(in Entity entity, in Layer layer, in string path, in IConfigurationRoot configuration)
         {
@@ -21,7 +17,7 @@ namespace Declaration.Generator.Internals.CodeTemplates
             var name = entity.GetTypeName().Type;
             var comment = Concat("Contains declaration describing ", name, " entity types");
             return new DeclarationFile(
-                GetTypeDeclaration(name, GetTypeNamespace(in path), in configuration, in comment, in UsingDirectiveList, FieldList(in entity)),
+                GetTypeDeclaration(name, GetTypeNamespace(in path), in configuration, in comment, in NamespaceImports, FieldList(in entity)),
                 path, Entities, name);
         }
     }
---
+++ factory/declaration/Declaration.Generator/Internals/CodeTemplates/DeclarationEntityUsingDirectiveList.cs
@@ -0,0 +1,12 @@
+﻿using DevOps.Primitives.CSharp;
+using DevOps.Primitives.CSharp.Helpers.Common;
+using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
+
+namespace Declaration.Generator.Internals.CodeTemplates
+{
+    internal static class DeclarationEntityUsingDirectiveList
+    {
+        public static readonly UsingDirectiveList NamespaceImports = Usings.Create(
+            UsingDirectives.UsingStatic("Declaration.Generator.Internals.DeclarationTypes.Helpers.Entities"));
+    }
+}
