69cff2ee44340f5b28a68127a97548a793f7fe3c
Christopher Dorst <chris@dorst.email>
2018-06-11 23:19:52 -0500

    Add ISqlStoredProcedureSavable interface

---
+++ Shared/ISqlStoredProcedureSavable.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared
+{
+    public interface ISqlStoredProcedureSavable
+    {
+        string ToSqlSaveProcedureParameters();
+        string ToSqlSaveProcedureParameters(in byte[] entityBytes);
+    }
+}
