9234d604f11732a8bbb26ede6f069f3e3185cfb7
Christopher Dorst <chris@dorst.email>
2018-06-11 23:20:35 -0500

    Add ISqlStoredProcedureSavable to IEntity

--- Shared/IEntity.cs
+++ Shared/IEntity.cs
@@ -2,7 +2,7 @@
 
 namespace Shared
 {
-    public interface IEntity<TEntity, TKey> : IEquatable<TEntity>, IMemorySerializable
+    public interface IEntity<TEntity, TKey> : IEquatable<TEntity>, IMemorySerializable, ISqlStoredProcedureSavable
         where TKey : unmanaged, IEquatable<TKey>
         where TEntity : IEntity<TEntity, TKey>
     {
