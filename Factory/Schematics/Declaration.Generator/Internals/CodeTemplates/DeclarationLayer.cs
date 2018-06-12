ec62d3acfe7e3b9a5a6a47dc04fdb0323424f967
Christopher Dorst <chris@dorst.email>
2018-06-11 21:51:00 -0500

    Add XML-doc & import directive to generated Layers types

--- Factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayer.cs
+++ Factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayer.cs
@@ -1,15 +1,19 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
+using DevOps.Primitives.CSharp;
 using Microsoft.Extensions.Configuration;
+using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
 
 namespace Declaration.Generator.Internals.CodeTemplates
 {
     internal class DeclarationLayer : DeclarationType
     {
         private const string Layer = nameof(Layer);
+        private static readonly UsingDirectiveList UsingDirectives = Usings.Create("System.Collections.Generic");
+        private static readonly string XmlDocSummary = "Responsible for enumerating constituent Entity types";
 
         public static DeclarationFile GetLayer(in Layer layer, in string path, in IConfigurationRoot configuration)
             => new DeclarationFile(
-                GetTypeDeclaration(Layer, GetTypeNamespace(in path), in configuration),
+                GetTypeDeclaration(Layer, GetTypeNamespace(in path), in configuration, in XmlDocSummary, in UsingDirectives),
                 path, Layer);
     }
 }
--- Factory/Schematics/Declaration/Layers/Block_00000_Primitives_00000_Identity/Layer.cs
+++ Factory/Schematics/Declaration/Layers/Block_00000_Primitives_00000_Identity/Layer.cs
@@ -1,8 +1,11 @@
 // Copyright © 2018 Christopher Dorst. All rights reserved.
 // Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
 
+using System.Collections.Generic;
+
 namespace Declaration.Layers.Block_00000_Primitives_00000_Identity
 {
+    /// <summary>Responsible for enumerating constituent Entity types</summary>
     public static class Layer
     {
     }
