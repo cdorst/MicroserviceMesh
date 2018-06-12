8925a23351ecc6edf7f0d781779a0b0d00b103fc
Christopher Dorst <chris@dorst.email>
2018-06-12 00:12:17 -0500

    Add EntryPoint static helper class to simplify ASP.NET Core Program.cs code for generated microservices

---
+++ Shared/Services/EntryPoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Shared.Services.Startups;
+using System;
+using static Microsoft.AspNetCore.WebHost;
+
+namespace Shared.Services
+{
+    public static class EntryPoint
+    {
+        public static void Service<TEntity, TKey>(in Operation operation, in string[] args = default)
+            where TEntity : IEntity<TEntity, TKey>
+            where TKey : unmanaged, IEquatable<TKey>
+        {
+            switch (operation)
+            {
+                case Operation.Save:
+                    Run<SaveOperationStartup<TEntity, TKey>>(in args);
+                    break;
+                default:
+                    throw new NotImplementedException(nameof(operation));
+            }
+        }
+
+        private static void Run<TStartup>(in string[] args)
+            where TStartup : class
+            => CreateDefaultBuilder(args)
+                .UseStartup<TStartup>()
+                .Build()
+                .Run();
+    }
+}
