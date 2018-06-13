1c920811c13a2b4c67892fdfb7ae9c0626a39f6c
Christopher Dorst <chris@dorst.email>
2018-06-13 08:32:25 -0500

    Consume generated Declaration.Root from microservice generator

--- factory/Generator/Program.cs
+++ factory/Generator/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Generator
+﻿using static Declaration.Root;
+
+namespace Generator
 {
     public static class Program
     {
         public static void Main(string[] args = default)
         {
+            var declaration = GetDeclaration();
+            foreach (var layer in declaration)
+            {
+                // TODO: Add layer to DbContext stack
+                // TODO: Add Save-operation microservice
+            }
         }
     }
 }
