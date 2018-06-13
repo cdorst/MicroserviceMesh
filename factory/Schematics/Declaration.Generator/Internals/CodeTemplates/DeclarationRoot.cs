0638cad9a3d1bf3a4a5a90bc343053d7389f00b1
Christopher Dorst <chris@dorst.email>
2018-06-13 02:59:08 -0500

    Add DeclarationRoot code generator types

---
+++ factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationRoot.cs
@@ -0,0 +1,19 @@
+﻿using Declaration.Generator.Internals.DeclarationTypes;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationRootMethodList;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationLayerRootUsings;
+
+namespace Declaration.Generator.Internals.CodeTemplates
+{
+    internal class DeclarationRoot : DeclarationType
+    {
+        private const string Root = nameof(Root);
+        private static readonly string XmlDocSummary = "Responsible for enumerating constituent Entity types";
+
+        public static DeclarationFile GetRoot(in IEnumerable<Layer> layers, in IConfigurationRoot configuration)
+            => new DeclarationFile(
+                GetTypeDeclaration(Root, ProjectName, in configuration, in XmlDocSummary, in UsingDirectives, methodList: GetMethodList(in layers)),
+                Root);
+    }
+}
---
+++ factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationRootMethodList.cs
@@ -0,0 +1,31 @@
+﻿using Declaration.Generator.Internals.DeclarationTypes;
+using DevOps.Primitives.CSharp;
+using DevOps.Primitives.CSharp.Helpers.Common;
+using System.Collections.Generic;
+using System.Linq;
+using static DevOps.Primitives.CSharp.Helpers.Common.Comments;
+using static DevOps.Primitives.CSharp.Helpers.Common.MethodLists;
+using static DevOps.Primitives.CSharp.Helpers.Common.Methods;
+using static System.String;
+
+namespace Declaration.Generator.Internals.CodeTemplates
+{
+    internal static class DeclarationRootMethodList
+    {
+        private const string Name = "GetDeclaration";
+        private const string Type = "IEnumerable<IEnumerable<Entity>>";
+        private static readonly DocumentationCommentList XmlDoc = Summary("Returns enumeration of Entity Layers");
+
+        public static MethodList GetMethodList(in IEnumerable<Layer> layers)
+            => Create(PublicStatic(Name, Type, BodyBlock(in layers), documentationCommentList: XmlDoc));
+
+        private static DevOps.Primitives.CSharp.Block BodyBlock(in IEnumerable<Layer> layers)
+            => Blocks.Create(BodyBlockStatements(layers).ToArray());
+
+        private static IEnumerable<string> BodyBlockStatements(IEnumerable<Layer> layers)
+        {
+            foreach (var layer in layers)
+                yield return Concat("yield return Layers.Block_", layer.BlockName, "_", layer.Name, ".Layer.GetEntities();");
+        }
+    }
+}
