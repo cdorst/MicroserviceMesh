da4b2dff9e47a0809087225cb14fbfb2b9830d49
Christopher Dorst <chris@dorst.email>
2018-06-13 02:25:04 -0500

    Refactor Layer MethodList responsibility

--- Factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayer.cs
+++ Factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayer.cs
@@ -1,13 +1,7 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
 using DevOps.Primitives.CSharp;
-using DevOps.Primitives.CSharp.Helpers.Common;
 using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
-using System.Linq;
-using static DevOps.Primitives.CSharp.Helpers.Common.Comments;
-using static DevOps.Primitives.CSharp.Helpers.Common.MethodLists;
-using static DevOps.Primitives.CSharp.Helpers.Common.Methods;
-using static System.String;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationLayerMethodList;
 using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
 
 namespace Declaration.Generator.Internals.CodeTemplates
@@ -15,9 +9,6 @@ namespace Declaration.Generator.Internals.CodeTemplates
     internal class DeclarationLayer : DeclarationType
     {
         private const string Layer = nameof(Layer);
-        private const string MethodName = "GetEntities";
-        private const string MethodType = "IEnumerable<Entity>";
-        private const string MethodXmlDoc = "Returns this Layer's Entity declarations";
         private static readonly UsingDirectiveList UsingDirectives = Usings.Create(
             "Declaration.Generator.Internals.DeclarationTypes",
             "System.Collections.Generic");
@@ -25,23 +16,7 @@ namespace Declaration.Generator.Internals.CodeTemplates
 
         public static DeclarationFile GetLayer(in Layer layer, in string path, in IConfigurationRoot configuration)
             => new DeclarationFile(
-                GetTypeDeclaration(Layer, GetTypeNamespace(in path), in configuration, in XmlDocSummary, in UsingDirectives, methodList: GetLayerMethodList(in layer)),
+                GetTypeDeclaration(Layer, GetTypeNamespace(in path), in configuration, in XmlDocSummary, in UsingDirectives, methodList: GetMethodList(in layer)),
                 path, Layer);
-
-        private static MethodList GetLayerMethodList(in Layer layer)
-            => Create(PublicStatic(MethodName, MethodType,
-                Blocks.Create(GetMethodBody(layer).ToArray()),
-                documentationCommentList: Summary(MethodXmlDoc)));
-
-        private static IEnumerable<string> GetMethodBody(Layer layer)
-        {
-            foreach (var entity in layer.Entities)
-            {
-                entity.BlockName = layer.BlockName;
-                entity.LayerName = layer.Name;
-                var entityName = entity.GetTypeName().Type;
-                yield return Concat("yield return ", entityName, ".Declaration;");
-            }
-        }
     }
 }
---
+++ factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayerMethodList.cs
@@ -0,0 +1,35 @@
+﻿using Declaration.Generator.Internals.DeclarationTypes;
+using DevOps.Primitives.CSharp;
+using DevOps.Primitives.CSharp.Helpers.Common;
+using System.Collections.Generic;
+using System.Linq;
+using static DevOps.Primitives.CSharp.Helpers.Common.Comments;
+using static DevOps.Primitives.CSharp.Helpers.Common.MethodLists;
+using static DevOps.Primitives.CSharp.Helpers.Common.Methods;
+using static System.String;
+
+namespace Declaration.Generator.Internals.CodeTemplates
+{
+    internal static class DeclarationLayerMethodList
+    {
+        private const string Name = "GetEntities";
+        private const string Type = "IEnumerable<Entity>";
+        private const string XmlDoc = "Returns this Layer's Entity declarations";
+
+        public static MethodList GetMethodList(in Layer layer)
+            => Create(PublicStatic(Name, Type,
+                Blocks.Create(GetMethodBody(layer).ToArray()),
+                documentationCommentList: Summary(XmlDoc)));
+
+        private static IEnumerable<string> GetMethodBody(Layer layer)
+        {
+            foreach (var entity in layer.Entities)
+            {
+                entity.BlockName = layer.BlockName;
+                entity.LayerName = layer.Name;
+                var entityName = entity.GetTypeName().Type;
+                yield return Concat("yield return ", entityName, ".Declaration;");
+            }
+        }
+    }
+}
