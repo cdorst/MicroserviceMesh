69171072d063dce1e17e0a1fb641561805c57f51
Christopher Dorst <chris@dorst.email>
2018-06-13 02:58:12 -0500

    Refactor Layer UsingDirectives responsibility (DeclarationRoot will reuse same list)

--- Factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayer.cs
+++ Factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayer.cs
@@ -1,17 +1,13 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
-using DevOps.Primitives.CSharp;
 using Microsoft.Extensions.Configuration;
 using static Declaration.Generator.Internals.CodeTemplates.DeclarationLayerMethodList;
-using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationLayerRootUsings;
 
 namespace Declaration.Generator.Internals.CodeTemplates
 {
     internal class DeclarationLayer : DeclarationType
     {
         private const string Layer = nameof(Layer);
-        private static readonly UsingDirectiveList UsingDirectives = Usings.Create(
-            "Declaration.Generator.Internals.DeclarationTypes",
-            "System.Collections.Generic");
         private static readonly string XmlDocSummary = "Responsible for enumerating constituent Entity types";
 
         public static DeclarationFile GetLayer(in Layer layer, in string path, in IConfigurationRoot configuration)
---
+++ factory/Schematics/Declaration.Generator/Internals/CodeTemplates/DeclarationLayerRootUsings.cs
@@ -0,0 +1,12 @@
+﻿using DevOps.Primitives.CSharp;
+using Usings = DevOps.Primitives.CSharp.Helpers.Common.UsingDirectiveLists;
+
+namespace Declaration.Generator.Internals.CodeTemplates
+{
+    internal static class DeclarationLayerRootUsings
+    {
+        public static readonly UsingDirectiveList UsingDirectives = Usings.Create(
+            "Declaration.Generator.Internals.DeclarationTypes",
+            "System.Collections.Generic");
+    }
+}
