ded1072c02117c12a748cd4c2fe267de370bd4fe
Christopher Dorst <chris@dorst.email>
2018-06-13 02:59:40 -0500

    Add DelcarationRoot file-writing responsibility

--- Factory/Schematics/Declaration.Generator/Internals/DeclarationCodeWriter.cs
+++ Factory/Schematics/Declaration.Generator/Internals/DeclarationCodeWriter.cs
@@ -1,6 +1,7 @@
 ﻿using Declaration.Generator.Internals.DeclarationTypes;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using static Declaration.Generator.Internals.CodeTemplates.DeclarationRoot;
 using static Declaration.Generator.Internals.DeclarationCodeGenerator;
 using static Declaration.Generator.Internals.DeclarationDeserializer;
 using static Declaration.Generator.Internals.FileWriter;
@@ -14,9 +15,21 @@ namespace Declaration.Generator.Internals
 
         private static void WriteFiles(in IEnumerable<Layer> layers, in IConfigurationRoot configuration)
         {
+            WriteLayers(in layers, in configuration);
+            WriteRoot(in layers, in configuration);
+        }
+
+        private static void WriteLayers(in IEnumerable<Layer> layers, in IConfigurationRoot configuration)
+        {
             foreach (var layer in layers)
                 foreach (var file in GenerateCode(layer, configuration))
                     WriteFile(in file.Contents, in file.PathParts);
+        }
+
+        private static void WriteRoot(in IEnumerable<Layer> layers, in IConfigurationRoot configuration)
+        {
+            var root = GetRoot(in layers, in configuration);
+            WriteFile(in root.Contents, in root.PathParts);
         }
     }
 }
